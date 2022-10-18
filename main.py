257c7d0ea6327ddc6e0506d21274129b89792c63
Raghav <70934347+raggzy271@users.noreply.github.com>
2022-10-18 14:12:17 +0530

    Built API

---
+++ main.py
@@ -0,0 +1,85 @@
+import librosa
+import numpy as np
+import pickle
+import statistics as st
+from fastapi import FastAPI, UploadFile
+import os
+
+app = FastAPI()
+
+@app.post('/identify-gender')
+async def identifyGender(audio: UploadFile):
+    # save the audio file
+    with open(audio.filename, 'wb') as f:
+        f.write(audio.file.read())
+
+    # get the features
+    features = get_features(audio.filename)
+
+    # delete the temporary file
+    os.remove(audio.filename)
+
+    # predict the gender from trained model
+    model_path = 'random_forest_model.sav'
+    with open(model_path , 'rb') as f:
+        model = pickle.load(f)
+        predictions = model.predict(features)
+    prediction = st.mode(predictions)    
+    return prediction
+
+def noise(data):
+    noise_amp = 0.035*np.random.uniform()*np.amax(data)
+    data = data + noise_amp*np.random.normal(size=data.shape[0])
+    return data
+
+def stretch(data, rate=0.8):
+    return librosa.effects.time_stretch(data, rate=rate)
+
+def pitch(data, sampling_rate, pitch_factor=0.7):
+    return librosa.effects.pitch_shift(data, sr=sampling_rate, n_steps=pitch_factor)
+
+def extract_features(data, sample_rate):
+    # ZCR
+    result = np.array([])
+    zcr = np.mean(librosa.feature.zero_crossing_rate(y=data).T, axis=0)
+    result=np.hstack((result, zcr)) # stacking horizontally
+
+    # Chroma_stft
+    stft = np.abs(librosa.stft(data))
+    chroma_stft = np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T, axis=0)
+    result = np.hstack((result, chroma_stft)) # stacking horizontally
+
+    # MFCC
+    mfcc = np.mean(librosa.feature.mfcc(y=data, sr=sample_rate).T, axis=0)
+    result = np.hstack((result, mfcc)) # stacking horizontally
+
+    # Root Mean Square Value
+    rms = np.mean(librosa.feature.rms(y=data).T, axis=0)
+    result = np.hstack((result, rms)) # stacking horizontally
+
+    # MelSpectogram
+    mel = np.mean(librosa.feature.melspectrogram(y=data, sr=sample_rate).T, axis=0)
+    result = np.hstack((result, mel)) # stacking horizontally
+    
+    return result
+
+def get_features(path):
+    # duration and offset are used to take care of the no audio in start and the ending of each audio files as seen above.
+    data, sample_rate = librosa.load(path, duration=2.5, offset=0.6)
+    
+    # without augmentation
+    res1 = extract_features(data, sample_rate)
+    result = np.array(res1)
+    
+    # data with noise
+    noise_data = noise(data)
+    res2 = extract_features(noise_data, sample_rate)
+    result = np.vstack((result, res2)) # stacking vertically
+    
+    # data with stretching and pitching
+    new_data = stretch(data)
+    data_stretch_pitch = pitch(new_data, sample_rate)
+    res3 = extract_features(data_stretch_pitch, sample_rate)
+    result = np.vstack((result, res3)) # stacking vertically
+    
+    return result
