c81d70cc76747302a67b15bae04338d14b06b116
Raghav <70934347+raggzy271@users.noreply.github.com>
2022-10-21 20:17:36 +0530

    add functionality to convert .3gp files to .wav

--- main.py
+++ main.py
@@ -9,23 +9,25 @@ voice_url = '/identify-voice'
 def index():
     return {"message": f"Hi, this is an API that identifies voices. You might want to send a POST request to {voice_url} to see us in action."}
 
-@app.post('/test-file')
-def test_file(file: UploadFile):
-    return {"success": True, "message": f"Received file {file.filename} of content type {file.content_type}.", "prediction": ""}
-
 @app.post(voice_url)
 def identify_voice(audio: UploadFile):
-    if len(audio.filename) == 0:
+    path_3gp = audio.filename
+    if len(path_3gp) == 0:
         return { "success": False, "message": "Please upload a valid audio file.", "prediction": "" }
 
     # save the audio file
-    with open(audio.filename, 'wb') as f:
+    with open(path_3gp, 'wb') as f:
         f.write(audio.file.read())
 
-    # get prediction
-    prediction = predict(audio.filename)    
+    # convert 3gp to wav file
+    path_wav = path_3gp.replace('.3gp', '.wav')
+    os.system(f"ffmpeg -i {path_3gp} {path_wav}")
 
-    # delete the temporary file
-    os.remove(audio.filename)
+    # get prediction
+    prediction = predict(path_wav)    
+
+    # delete the files
+    os.remove(path_3gp)
+    os.remove(path_wav)
 
     return { "success": True, "message": "Prediction successful", "prediction": prediction }
