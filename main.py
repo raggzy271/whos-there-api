cf26611e617b41a7bc915987271bce160e471d28
Raghav <70934347+raggzy271@users.noreply.github.com>
2022-10-21 20:57:27 +0530

    Make the test function into main

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ voice_url = '/identify-voice'
 def index():
     return {"message": f"Hi, this is an API that identifies voices. You might want to send a POST request to {voice_url} to see us in action."}
 
-@app.post('/test-file')
-async def test_file(file: UploadFile):
-    path_3gp = file.filename
+@app.post(voice_url)
+async def identify_voice(audio: UploadFile):
+    path_3gp = audio.filename
     if len(path_3gp) == 0:
         return { "success": False, "message": "Please upload a valid audio file.", "prediction": "" }
 
     # save the audio file
     with open(path_3gp, 'wb') as f:
-        f.write(file.file.read())
+        f.write(audio.file.read())
 
     # convert 3gp to wav file
     path_wav = path_3gp.replace('.3gp', '.wav')
